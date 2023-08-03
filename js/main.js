bacc156ed3aaa393edfd2c363c4235b4e961e4ca
Daedaluzz <arthurbernardpereira@gmail.com>
2023-08-03 04:14:47 -0300

    index concluída, footer pronto

--- js/main.js
+++ js/main.js
@@ -57,3 +57,23 @@ for (let i = 0; i < animation_elements2.length; i++) {
 
 	observer2.observe(el);
 }
+
+//divisorias footer
+const animation_elements3 = document.querySelectorAll('.ani3');
+const observer3 = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('animate3');
+		} else {
+			entry.target.classList.remove('animate3');
+		}
+	})
+}, {
+	threshold: 0.5
+});
+
+for (let i = 0; i < animation_elements2.length; i++) {
+	const el = animation_elements3[i];
+
+	observer3.observe(el);
+}
